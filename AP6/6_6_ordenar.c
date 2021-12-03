dcd1c202803e8eb12a3ce7827b6b3c69331938dc
Pedro <73082644+Kyunha@users.noreply.github.com>
2021-12-03 09:34:33 +0000

    Update 6_6_ordenar.c

--- AP6/6_6_ordenar.c
+++ AP6/6_6_ordenar.c
@@ -2,7 +2,8 @@
 
 
 void bubblesort(float x[], int n) {
-    int i, j, y;
+    int i, j;
+    float y;
     for (i = 0; i < (n - 1); ++i){
         for (j = 0; j < n - 1 - i; ++j ){
             if (x[j] > x[j+1]) {
@@ -15,15 +16,16 @@ int main() {
 	int n;
 	float x[15], y;
 	printf("Quantos elementos pretende armazenar? ");scanf("%d",&n);
-	printf("Introduza o elemento 1: ");scanf("%f",&x[0]);
-	for (int i = 1; i < n; i++) {
+	//printf("Introduza o elemento 1: ");scanf("%f",&x[0]);
+	for (int i = 0; i < n; i++) {
 		printf("Introduza o elemento %d: ",i+1);scanf("%f",&x[i]);
-		bubblesort(x,i);
+		bubblesort(x,i+1);
 		printf("{ ");
-		for (int j = 0; j < i; ++j)
+		for (int j = 0; j <= i; ++j)
 		{
 			printf("%.1f ",x[j]);
 		}printf("}\n");
 	}
+
 	return 0;
 }
